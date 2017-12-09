662ee16bea3808726eb8b80ed2f5fb70ada0e5bc
ali moussawi <aloush.aam@gmail.com>
2017-12-09 13:01:07 +0200

    simple adding and subtracting

---
+++ ostpc-essentials/src/main/java/ua/nure/ostpc/NewFile.java
@@ -0,0 +1,11 @@
+package ua.nure.ostpc;
+
+public class NewFile {
+    public static void main (String[]args){
+       System.out.println(" just creating some files ");
+
+        }
+        public int calc(int a , int b , int c){
+        return a+b-c;
+    }
+}
