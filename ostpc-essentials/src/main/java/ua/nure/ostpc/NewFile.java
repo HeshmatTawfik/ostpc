8b788cba333fd61315039d32513a915629d97da3
Heshmat Tawfik <johnjohn10151john@gmail.com>
2017-12-09 13:25:31 +0200

    change to **

--- ostpc-essentials/src/main/java/ua/nure/ostpc/NewFile.java
+++ ostpc-essentials/src/main/java/ua/nure/ostpc/NewFile.java
@@ -1,11 +1,11 @@
 package ua.nure.ostpc;
 
 public class NewFile {
-    public static void main (String[]args){
-       System.out.println(" just creating some files ");
+    public static void main(String[] args) {
+        System.out.println(" just creating some files ");
+    }
 
-        }
-        public int dvi2d(int a , int b , int c){
-        return a*b-c;
+    public int dvi2d(int a, int b, int c) {
+        return a * b - c;
     }
 }
